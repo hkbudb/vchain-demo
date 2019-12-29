9145ec4e8a847e6560629935cccdd04a95182d6d
Cheng XU <git@xuc.me>
2019-12-30 06:26:37 +0800

    digest_set: expand_to_poly

--- src/acc/digest_set.rs
+++ src/acc/digest_set.rs
@@ -1,7 +1,9 @@
 use crate::acc::field::Fr;
 use crate::acc::utils::digest_to_fr;
 use crate::set::{MultiSet, SetElement};
-use rayon::prelude::*;
+use algebra::Field;
+use ff_fft::DensePolynomial;
+use rayon::{self, prelude::*};
 
 #[derive(Debug, Clone, Default)]
 pub struct DigestSet {
@@ -22,5 +24,47 @@ impl DigestSet {
         Self { inner }
     }
 
-    pub fn expand_to_poly(&self) {}
+    pub fn expand_to_poly(&self) -> DensePolynomial<Fr> {
+        let mut inputs = Vec::new();
+        for (k, v) in &self.inner {
+            for _ in 0..*v {
+                inputs.push(DensePolynomial::from_coefficients_vec(vec![*k, Fr::one()]));
+            }
+        }
+        fn expand(polys: &[DensePolynomial<Fr>]) -> DensePolynomial<Fr> {
+            if polys.is_empty() {
+                return DensePolynomial::from_coefficients_vec(vec![Fr::one()]);
+            } else if polys.len() == 1 {
+                return polys[0].clone();
+            }
+            let mid = polys.len() / 2;
+            let (left, right) = rayon::join(|| expand(&polys[..mid]), || expand(&polys[mid..]));
+            &left * &right
+        }
+        expand(&inputs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_digest_to_poly() {
+        let set = DigestSet {
+            inner: vec![
+                (Fr::from(1u32), 2),
+                (Fr::from(2u32), 1),
+                (Fr::from(3u32), 1),
+            ],
+        };
+        let expect = DensePolynomial::from_coefficients_vec(vec![
+            Fr::from(6u32),
+            Fr::from(17u32),
+            Fr::from(17u32),
+            Fr::from(7u32),
+            Fr::from(1u32),
+        ]);
+        assert_eq!(set.expand_to_poly(), expect);
+    }
 }
