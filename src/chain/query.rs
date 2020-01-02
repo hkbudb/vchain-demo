807102ed9e317327334e5594796b9efddd1cf7ca
Cheng XU <git@xuc.me>
2020-01-03 02:52:07 +0800

    query to/from json

--- src/chain/query.rs
+++ src/chain/query.rs
@@ -1,8 +1,10 @@
 use super::{IdType, SetElementType};
 use crate::set::{MultiSet, SetElement};
+use anyhow::{bail, Context, Error, Result};
 use core::iter::FromIterator;
 use core::ops::Deref;
 use serde::{Deserialize, Serialize};
+use serde_json::{json, value::Value as JsonValue};
 use std::collections::{HashSet, VecDeque};
 
 #[derive(Debug, Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
@@ -48,12 +50,50 @@ impl<T: SetElement> FromIterator<MultiSet<T>> for BoolExp<T> {
 pub struct Range(pub(crate) [Vec<Option<u32>>; 2]);
 
 impl Range {
+    pub fn from_json(input: &JsonValue) -> Result<Self> {
+        let err_msg = format!("invald input: {:?}", input);
+        let p1 = input
+            .get(0)
+            .context(err_msg.clone())?
+            .as_array()
+            .context(err_msg.clone())?
+            .iter()
+            .map(|v| v.as_u64().map(|v| v as u32))
+            .collect::<Vec<_>>();
+        let p2 = input
+            .get(1)
+            .context(err_msg.clone())?
+            .as_array()
+            .context(err_msg)?
+            .iter()
+            .map(|v| v.as_u64().map(|v| v as u32))
+            .collect::<Vec<_>>();
+        Ok(Self([p1, p2]))
+    }
+
+    pub fn to_json(&self) -> JsonValue {
+        let p1: Vec<JsonValue> = self.0[0]
+            .iter()
+            .map(|v| match v {
+                Some(v) => json!(v),
+                None => json!(null),
+            })
+            .collect();
+        let p2: Vec<JsonValue> = self.0[1]
+            .iter()
+            .map(|v| match v {
+                Some(v) => json!(v),
+                None => json!(null),
+            })
+            .collect();
+        json!([p1, p2])
+    }
+
     pub fn to_bool_exp(&self, bit_len: &[u8]) -> BoolExp<SetElementType> {
         let mut exp = BoolExp::new();
-        let dim = self[0].len();
-        for i in 0..dim {
-            let (l, r) = match (self[0][i], self[1][i]) {
-                (Some(x), Some(y)) => (x, y),
+        for (i, range) in self[0].iter().zip(self[1].iter()).enumerate() {
+            let (l, r) = match (range.0, range.1) {
+                (Some(x), Some(y)) => (*x, *y),
                 _ => continue,
             };
 
@@ -110,6 +150,66 @@ pub struct Query {
 }
 
 impl Query {
+    pub fn from_json(input: &JsonValue) -> Result<Self> {
+        let err_msg = format!("invald input: {:?}", input);
+        let start_block = input
+            .get("start_block")
+            .context(err_msg.clone())?
+            .as_u64()
+            .context(err_msg.clone())? as IdType;
+        let end_block = input
+            .get("end_block")
+            .context(err_msg.clone())?
+            .as_u64()
+            .context(err_msg.clone())? as IdType;
+        let q_range = match input.get("range") {
+            Some(range) => Some(Range::from_json(range)?),
+            None => None,
+        };
+        let q_bool = match input.get("bool") {
+            Some(exp) => {
+                let parsed_exp = match exp.as_array() {
+                    Some(exp_array) => exp_array
+                        .iter()
+                        .map(|sub_array| match sub_array.as_array() {
+                            Some(sub_array) => sub_array
+                                .iter()
+                                .map(|s| s.as_str().map(|s| s.to_owned()).context(err_msg.clone()))
+                                .collect::<Result<HashSet<_>>>(),
+                            None => Err(Error::msg(err_msg.clone())),
+                        })
+                        .collect::<Result<Vec<_>>>()?,
+                    None => bail!(err_msg),
+                };
+                Some(parsed_exp)
+            }
+            None => None,
+        };
+        Ok(Self {
+            start_block,
+            end_block,
+            q_range,
+            q_bool,
+        })
+    }
+
+    pub fn to_json(&self) -> JsonValue {
+        let mut res = json!({
+            "start_block": self.start_block,
+            "end_block": self.end_block,
+        });
+        if let Some(q_range) = &self.q_range {
+            res["range"] = q_range.to_json();
+        }
+        if let Some(q_bool) = &self.q_bool {
+            res["bool"] = json!(q_bool
+                .iter()
+                .map(|sub_exp| sub_exp.iter().map(|w| json!(w)).collect::<Vec<_>>())
+                .collect::<Vec<_>>());
+        }
+        res
+    }
+
     pub fn to_bool_exp(&self, bit_len: &[u8]) -> BoolExp<SetElementType> {
         let mut exp = BoolExp::new();
         if let Some(q_range) = &self.q_range {
@@ -117,9 +217,9 @@ impl Query {
                 .extend(q_range.to_bool_exp(bit_len).iter().cloned());
         }
         if let Some(q_bool) = &self.q_bool {
-            for s in q_bool.iter() {
+            for sub_exp in q_bool.iter() {
                 exp.inner.push(MultiSet::from_iter(
-                    s.iter().map(|w| SetElementType::W(w.clone())),
+                    sub_exp.iter().map(|w| SetElementType::W(w.clone())),
                 ));
             }
         }
@@ -161,5 +261,38 @@ mod tests {
             ]),
         ]);
         assert_eq!(range.to_bool_exp(&[3, 3, 3]), expect);
+    }
+
+    #[test]
+    fn test_query() {
+        let data = json!({
+            "start_block": 1,
+            "end_block": 2,
+            "range": [
+                [0, null, 3],
+                [6, null, 4],
+            ],
+            "bool": [
+                ["a", "b"],
+                ["c"],
+            ],
+        });
+        let expect = Query {
+            start_block: 1,
+            end_block: 2,
+            q_range: Some(Range([
+                vec![Some(0), None, Some(3)],
+                vec![Some(6), None, Some(4)],
+            ])),
+            q_bool: Some(vec![
+                ["a".to_owned(), "b".to_owned()]
+                    .iter()
+                    .cloned()
+                    .collect::<HashSet<_>>(),
+                ["c".to_owned()].iter().cloned().collect::<HashSet<_>>(),
+            ]),
+        };
+        assert_eq!(Query::from_json(&data).unwrap(), expect);
+        assert_eq!(data, expect.to_json());
     }
 }
